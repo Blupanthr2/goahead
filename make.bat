328da474185f43578a21bd6af09100aef44d7287
Embedthis Development <dev@embedthis.com>
2026-04-26 08:11:18 +1000

    DEV: update build system

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
 @echo off
-REM Build Ioto using pre-generated VS2022 projects
+REM Build GoAhead using pre-generated VS2022 projects
 projects\windows.bat msbuild projects\vs2022\goahead.sln /p:Configuration=Debug %1 %2 %3 %4 %5 %6 %7 %8 %9
